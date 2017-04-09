1f8b7770fc61f0bdb6884ae8fd341354f20d0d99
rwscherrer <rwscherrer@gmail.com>
2017-04-09 11:46:04 -0500

    fixed_devise_route_for_sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/registrations' }
 
   devise_scope :user do
     get 'register', to: 'users/registrations#new'
