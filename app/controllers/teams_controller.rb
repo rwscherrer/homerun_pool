d1d06792098c5fd28b2cc7a2d8a4de082da607b3
rwscherrer <rwscherrer@gmail.com>
2017-04-12 17:34:26 -0500

    added_readline_gem

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -121,7 +121,7 @@ class TeamsController < ApplicationController
 	    player_9_data: Player.find_by(name: @team.player_9).home_runs_2017,
 
 	    })
-	 
+	  
 	    flash[:success] = [ "Team Updated." ]
 	    redirect_to '/teams'
 	  else
